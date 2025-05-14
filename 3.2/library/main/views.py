b8813606e2eaed10e6a80efe0d082b30c4fc3d38
Viktoriya Lysova <vikulya.lysova@list.ru>
2025-05-14 20:04:38 +0300

    Homework done

--- 3.2/library/main/views.py
+++ 3.2/library/main/views.py
@@ -1,11 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.generics import RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from main.models import Book, Order
-from main.serializers import BookSerializer
+from main.serializers import BookSerializer, OrderSerializer
 
 
 @api_view(['GET'])
@@ -13,32 +13,38 @@ def books_list(request):
     """получите список книг из БД
     отсериализуйте и верните ответ
     """
-    return Response(...)
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
 
 
 class CreateBookView(APIView):
     def post(self, request):
         # получите данные из запроса
-        serializer = BookSerializer(...) #передайте данные из запроса в сериализатор
+        data = request.data
+        serializer = BookSerializer(data=data) #передайте данные из запроса в сериализатор
         if serializer.is_valid(raise_exception=True): #если данные валидны
+            serializer.save()
             return Response('Книга успешно создана') # возвращаем ответ об этом
 
 
 class BookDetailsView(RetrieveAPIView):
     # реализуйте логику получения деталей одного объявления
-    ...
-
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookUpdateView(UpdateAPIView):
     # реализуйте логику обновления объявления
-    ...
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
 class BookDeleteView(DestroyAPIView):
     # реализуйте логику удаления объявления
-    ...
-
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class OrderViewSet(viewsets.ModelViewSet):
     # реализуйте CRUD для заказов
-    ...
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
